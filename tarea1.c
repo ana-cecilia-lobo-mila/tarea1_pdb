eaa1aa72d605d28058ae9d57396cd37d830f076b
Ana Lobo <anacl.1935@gmail.com>
2026-03-26 16:53:55 -0300

    Avance en cesar y morse

--- tarea1.c
+++ tarea1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+char alfabeto[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m', 'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char *morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+void leer_texto();
 void funcion1();
 void funcion2();
 void funcion3();
@@ -36,9 +40,34 @@ int menu(int valor){
     return valor;
 }
 
+void leer_texto(){
+
+}
+
+
 void funcion1(){
-    printf("Funcion 1\n");
-    main();
+    char texto[100];
+    printf("Texto a código morse seleccionado. \n");
+    printf("Ingrese el texto de entrada a continuación: \n");
+    getchar();
+    fgets(texto, 100, stdin);
+    char *texto_convertido[100];
+
+    for(int i = 0; texto[i] != '\0'; i++){
+        for(int j = 0; j < 26 ; j++){
+            if(alfabeto[j] == texto[i]){
+                texto_convertido[i] = morse[j];
+                break;
+            }
+
+        }
+    }
+
+    printf("El texto %s en código morse es: ", texto);
+    for(int i = 0; texto_convertido[i] != NULL; i++){
+        printf("%s ", texto_convertido[i]);
+    }
+    //main();
     
 }
 
@@ -48,8 +77,37 @@ void funcion2(){
 }
 
 void funcion3(){
-    printf("Funcion 3\n");
-    main();
+    char texto[100];
+    int desplazo;
+
+    printf("Texto a texto desplazado seleccionado.\n");
+    printf("Ingrese el texto de entrada a continuación: \n");
+    getchar();
+    fgets(texto, 100, stdin);
+    printf("Ingrese el número de letras a desplazar: \n");
+    scanf("%d", &desplazo);
+
+    printf("%s", texto);
+
+    char resultado[100];
+    int i; 
+
+    for (i = 0; texto[i] != '\0'; i++){
+        if (texto[i] >= 'a' && texto[i] <= 'z'){
+            resultado[i] = (texto[i] - 'a' + desplazo % 26 + 26) % 26 + 'a';
+        }else if (texto[i] >= 'A' && texto[i] <= 'Z'){
+            resultado[i] = (texto[i] - 'A' + desplazo % 26 + 26) % 26 + 'A';
+        }else{
+            resultado[i] = texto[i];
+            }
+    }
+    
+    resultado[i] = '\0';
+
+    printf("El texto %s desplazado por %d letras es: ", texto, desplazo);
+    printf("%s", resultado);
+
+    //main();
 }
 
 void funcion4(){
