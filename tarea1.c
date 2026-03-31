604cfa3493636b9d0a584276475da19e7719ded6
Ana Lobo <anacl.1935@gmail.com>
2026-03-31 14:13:04 -0300

    Casi final

--- tarea1.c
+++ tarea1.c
@@ -4,6 +4,8 @@
 char alfabeto[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m', 'n','o','p','q','r','s','t','u','v','w','x','y','z'};
 char alfabetoMayus[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char morse[][5] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+unsigned char acentos[] = {161, 169, 173, 179, 186, 177, 129, 137, 141, 147, 154, 145};
+char letras[] = {'a','e','i','o','u','ñ','A','E','I','O', 'U','Ñ'};
 
 void funcion1();
 void funcion2();
@@ -13,18 +15,21 @@ int menu(int valor);
 
 int main(){
     int valor;
+    char buffer[10];
+
     printf("Bienvenido. Seleccione un método de conversión\n");
     printf("1) Texto a código morse.\n");
     printf("2) Código morse a texto.\n");
     printf("3) Texto desplazado.\n");
     printf("4) Diccionario de reemplazo.\n");
-    scanf("%d", &valor);
+
+    fgets(buffer, 10, stdin);
+    valor = buffer[0] - '0';
 
     menu(valor);
 
     return 0;
 }
-
 
 int menu(int valor){
     if(valor == 1){
@@ -37,11 +42,51 @@ int menu(int valor){
         funcion4();
     } else{
         printf("Número no válido\n");
-        //main()
     }
     return valor;
 }
 
+int tiene_repetidos(char str[]) {
+    for(int i = 0; str[i] != '\0'; i++){
+        for(int j = i + 1; str[j] != '\0'; j++){
+            if(str[i] == str[j]){
+                return 1; 
+            }
+        }
+    }
+    return 0;
+}
+
+int leer_entero() {
+    char buffer[100];
+    int numero = 0;
+    int i = 0;
+    int signo = 1;
+
+    fgets(buffer, 100, stdin);
+
+    if(buffer[0] == '-'){
+        signo = -1;
+        i++;
+    }
+
+    for(; buffer[i] != '\n' && buffer[i] != '\0'; i++){
+        if(buffer[i] >= '0' && buffer[i] <= '9'){
+            numero = numero * 10 + (buffer[i] - '0');
+        }
+    }
+
+    return numero * signo;
+}
+
+char normalizar_acento(unsigned char segundo_byte) {
+    for (int k = 0; k < 12; k++) {
+        if (segundo_byte == acentos[k]) {
+            return letras[k];
+        }
+    }
+    return '\0';
+}
 
 void funcion1(){
     char nombre_archivo[100];
@@ -49,10 +94,8 @@ void funcion1(){
 
     printf("Texto a código morse seleccionado. \n");
     printf("Ingrese el nombre del archivo de entrada que contiene el mensaje: \n");
-    getchar();
     fgets(nombre_archivo, 100, stdin);
 
-    // eliminar salto de línea
     for(int i = 0; nombre_archivo[i] != '\0'; i++){
         if(nombre_archivo[i] == '\n'){
             nombre_archivo[i] = '\0';
@@ -65,17 +108,12 @@ void funcion1(){
     if(archivo == NULL){
         printf("El archivo no existe\n");
         printf("Ingrese el texto de entrada a continuación: \n");
-        getchar();
         fgets(texto, 1000, stdin);
-
         texto[strcspn(texto, "\n")] = '\0';
     }else{
-        // leer contenido
         int i = 0;
         while(fgets(texto + i, 1000 - i, archivo) != NULL){
-            while(texto[i] != '\0'){
-                i++;
-            }
+            while(texto[i] != '\0') i++;
         }
         fclose(archivo);
     }
@@ -86,7 +124,7 @@ void funcion1(){
     for(int i = 0; texto[i] != '\0'; i++){
 
         if(texto[i] == '\n'){
-            strcpy(texto_convertido[i], " "); 
+            strcpy(texto_convertido[i], " ");
             continue;
         }
 
@@ -96,23 +134,42 @@ void funcion1(){
         }
 
         if(texto[i] >= 'a' && texto[i] <= 'z'){
-            for(int j = 0; j < 26 ; j++){
+            for(int j = 0; j < 26; j++){
                 if(alfabeto[j] == texto[i]){
                     strcpy(texto_convertido[i], morse[j]);
                     break;
                 }
-            } 
-        }else if(texto[i] >= 'A' && texto[i] <= 'Z'){
-            for(int j = 0; j < 26 ; j++){
+            }
+        } else if(texto[i] >= 'A' && texto[i] <= 'Z'){
+            for(int j = 0; j < 26; j++){
                 if(alfabetoMayus[j] == texto[i]){
                     strcpy(texto_convertido[i], morse[j]);
                     break;
                 }
-            } 
-        }else{
+            }
+        } else if((unsigned char)texto[i] >= 128){
+            char letra = normalizar_acento((unsigned char)texto[i + 1]);
+            int encontrado = 0;
+
+            if(letra != '\0'){
+                for(int j = 0; j < 26; j++){
+                    if(alfabeto[j] == letra || alfabetoMayus[j] == letra){
+                        strcpy(texto_convertido[i], morse[j]);
+                        encontrado = 1;
+                        break;
+                    }
+                }
+            }
+
+            if(!encontrado){
+                texto_convertido[i][0] = texto[i];
+                texto_convertido[i][1] = texto[i + 1];
+                texto_convertido[i][2] = '\0';
+            }
+            i++; 
+        } else {
             especiales[i][0] = texto[i];
             especiales[i][1] = '\0';
-
             strcpy(texto_convertido[i], especiales[i]);
         }
     }
@@ -123,7 +180,6 @@ void funcion1(){
     }
     printf("\n");
     main();
-    
 }
 
 void funcion2(){
@@ -132,10 +188,8 @@ void funcion2(){
 
     printf("Código morse a texto seleccionado. \n");
     printf("Ingrese el nombre del archivo de entrada que contiene el mensaje: \n");
-    getchar();
     fgets(nombre_archivo, 100, stdin);
 
-    // eliminar salto de línea
     for(int i = 0; nombre_archivo[i] != '\0'; i++){
         if(nombre_archivo[i] == '\n'){
             nombre_archivo[i] = '\0';
@@ -148,57 +202,51 @@ void funcion2(){
     if(archivo == NULL){
         printf("El archivo no existe\n");
         printf("Ingrese el texto de entrada a continuación: \n");
-        getchar();
         fgets(texto, 1000, stdin);
-
         texto[strcspn(texto, "\n")] = '\0';
     }else{
-        // leer contenido
         int i = 0;
         while(fgets(texto + i, 1000 - i, archivo) != NULL){
-            while(texto[i] != '\0'){
-                i++;
-            }
+            while(texto[i] != '\0') i++;
         }
         fclose(archivo);
     }
 
     char texto_convertido[1000];
+    int i = 0, x = 0, y = 0;
 
-    int i = 0;
-    int x = 0;
-    int y = 0;
     while(texto[x] != '\0'){
         char letra[100];
         int k = 0;
 
         if(texto[x] == '/'){
-            texto_convertido[y] = ' ';
-            y++;
+            texto_convertido[y++] = ' ';
             x++;
-        }else if(texto[x] == ' '){
-            x++; 
-        }else if(texto[x] != '.' && texto[x] != '-'){
-            texto_convertido[y] = texto[x];
-            y++;
+        } else if(texto[x] == ' '){
             x++;
-        }else{
+        } else if(texto[x] != '.' && texto[x] != '-'){
+            texto_convertido[y++] = texto[x++];
+        } else {
             for(i = x; texto[i] != ' '; i++){
-                letra[k] = texto[i];
-                k++;
+                letra[k++] = texto[i];
             }
             letra[k] = '\0';
-            
 
-            for(int j = 0; j < 26 ; j++){
+            int encontrado = 0;
+            for(int j = 0; j < 26; j++){
                 if(strcmp(morse[j], letra) == 0){
-                    texto_convertido[y] = alfabeto[j];
-                    y++;
+                    texto_convertido[y++] = alfabeto[j];
+                    encontrado = 1;
                     break;
                 }
             }
+
+            if(!encontrado){
+                printf("Código morse no válido\n");
+                return main();
+            }
             x = i + 1;
-        }   
+        }
     }
 
     texto_convertido[y] = '\0';
@@ -212,16 +260,13 @@ void funcion2(){
 }
 
 void funcion3(){
-
     char nombre_archivo[100];
     char texto[1000];
 
     printf("Texto a texto desplazado seleccionado.\n");
     printf("Ingrese el nombre del archivo de entrada que contiene el mensaje: \n");
-    getchar();
     fgets(nombre_archivo, 100, stdin);
 
-    // eliminar salto de línea
     for(int i = 0; nombre_archivo[i] != '\0'; i++){
         if(nombre_archivo[i] == '\n'){
             nombre_archivo[i] = '\0';
@@ -234,45 +279,49 @@ void funcion3(){
     if(archivo == NULL){
         printf("El archivo no existe\n");
         printf("Ingrese el texto de entrada a continuación: \n");
-        getchar();
         fgets(texto, 1000, stdin);
-
         texto[strcspn(texto, "\n")] = '\0';
     }else{
-        // leer contenido
         int i = 0;
         while(fgets(texto + i, 1000 - i, archivo) != NULL){
-            while(texto[i] != '\0'){
-                i++;
-            }
+            while(texto[i] != '\0') i++;
         }
-
         fclose(archivo);
-
     }
-
-    int desplazo;
 
     printf("Ingrese el número de letras a desplazar: \n");
-    scanf("%d", &desplazo);
+    int desplazo = leer_entero();
 
-    char resultado[100];
-    int i; 
+    char texto_convertido[1000];
+    int y = 0;
 
-    for (i = 0; texto[i] != '\0'; i++){
-        if (texto[i] >= 'a' && texto[i] <= 'z'){
-            resultado[i] = (texto[i] - 'a' + desplazo % 26 + 26) % 26 + 'a';
-        }else if (texto[i] >= 'A' && texto[i] <= 'Z'){
-            resultado[i] = (texto[i] - 'A' + desplazo % 26 + 26) % 26 + 'A';
-        }else{
-            resultado[i] = texto[i];
+    for(int i = 0; texto[i] != '\0'; i++){
+        if(texto[i] >= 'a' && texto[i] <= 'z'){
+            texto_convertido[y++] = (texto[i] - 'a' + desplazo % 26 + 26) % 26 + 'a';
+
+        } else if(texto[i] >= 'A' && texto[i] <= 'Z'){
+            texto_convertido[y++] = (texto[i] - 'A' + desplazo % 26 + 26) % 26 + 'A';
+
+        } else if((unsigned char)texto[i] >= 128){
+            char letra = normalizar_acento((unsigned char)texto[i + 1]);
+
+            if(letra >= 'a' && letra <= 'z'){
+                texto_convertido[y++] = (letra - 'a' + desplazo % 26 + 26) % 26 + 'a';
+            } else if(letra >= 'A' && letra <= 'Z'){
+                texto_convertido[y++] = (letra - 'A' + desplazo % 26 + 26) % 26 + 'A';
+            } else if(letra != '\0'){
+                texto_convertido[y++] = letra;
             }
+            i++;
+        } else {
+            texto_convertido[y++] = texto[i];
+        }
     }
-    
-    resultado[i] = '\0';
+
+    texto_convertido[y] = '\0';
 
     printf("El texto %s desplazado por %d letras es: ", texto, desplazo);
-    printf("%s", resultado);
+    printf("%s \n", texto_convertido);
 
     main();
 }
@@ -283,10 +332,8 @@ void funcion4(){
 
     printf("Texto a diccionario de reemplazo seleccionado \n");
     printf("Ingrese el nombre del archivo de entrada que contiene el mensaje: \n");
-    getchar();
     fgets(nombre_archivo, 100, stdin);
 
-    // eliminar salto de línea
     for(int i = 0; nombre_archivo[i] != '\0'; i++){
         if(nombre_archivo[i] == '\n'){
             nombre_archivo[i] = '\0';
@@ -299,34 +346,39 @@ void funcion4(){
     if(archivo == NULL){
         printf("El archivo no existe\n");
         printf("Ingrese el texto de entrada a continuación: \n");
-        getchar();
         fgets(texto, 1000, stdin);
-
         texto[strcspn(texto, "\n")] = '\0';
     }else{
-        // leer contenido
         int i = 0;
         while(fgets(texto + i, 1000 - i, archivo) != NULL){
-            while(texto[i] != '\0'){
-                i++;
-            }
+            while(texto[i] != '\0') i++;
         }
-
         fclose(archivo);
-
     }
+
     char cambio[100];
 
     printf("Ingrese el diccionario de caracteres a reemplazar \n");
     printf("FORMATO: ENTRADA:SALIDA \n");
     printf("Sin repetir caracteres \n");
 
-    
     fgets(cambio, 100, stdin);
+
+    int tiene_dos_puntos = 0;
+    for(int i = 0; cambio[i] != '\0'; i++){
+        if(cambio[i] == ':'){
+            tiene_dos_puntos = 1;
+            break;
+        }
+    }
+
+    if(!tiene_dos_puntos){
+        printf("Formato no valido \n");
+        main();
+    }
 
     char diccionario_entrada[100];
     char diccionario_salida[100];
-
     int i = 0;
 
     while(cambio[i] != ':'){
@@ -338,46 +390,52 @@ void funcion4(){
     int j = 0;
 
     while(cambio[i] != '\0' && cambio[i] != '\n'){
-        diccionario_salida[j] = cambio[i];
-        i++;
-        j++;
+        diccionario_salida[j++] = cambio[i++];
     }
     diccionario_salida[j] = '\0';
-    
+
+    if(strlen(diccionario_entrada) != strlen(diccionario_salida) || tiene_repetidos(diccionario_entrada) == 1 || tiene_repetidos(diccionario_salida) == 1){
+        printf("Diccionario no válido \n");
+        main();
+    }
+
     char texto_convertido[100];
     int y = 0;
+
     for(int k = 0; texto[k] != '\0'; k++){
         int encontrado = 0;
 
-        for(int x = 0; x < strlen(diccionario_entrada); x++){
+        for(int x = 0; x < (int)strlen(diccionario_entrada); x++){
             if(texto[k] == diccionario_entrada[x]){
-                texto_convertido[y] = diccionario_salida[x];
-                y++;
+                texto_convertido[y++] = diccionario_salida[x];
                 encontrado = 1;
                 break;
-            }else if(texto[k] == diccionario_salida[x]){
-                texto_convertido[y] = diccionario_entrada[x];
-                y++;
+            } else if(texto[k] == diccionario_salida[x]){
+                texto_convertido[y++] = diccionario_entrada[x];
                 encontrado = 1;
                 break;
             }
         }
         if(!encontrado){
-            texto_convertido[y] = texto[k];
-            y++;
+            texto_convertido[y++] = texto[k];
         }
     }
     texto_convertido[y] = '\0';
 
     printf("El texto %s cifrado mediante el diccionario proporcionado es: ", texto);
     printf("%s \n", texto_convertido);
+
+    main();
 }
 
 //gcc tarea1.c -o 
 //.... --- .-.. .- / .- -. .- !!!
 //an:hl
 
-//Hola, ¿Cómo estás?
-//Coleá?:Wad4R.
+//Hola, ¿Como estas?
+//Colea?:Wad4R.
 
 //../archivo.txt
+//´áéíóú
+//Holá Ana
+
